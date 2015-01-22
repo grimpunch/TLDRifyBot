dad7a0ef54cf47a2a6ae703a29fa8c8512347aac
Christian Murphy <christheromguy@googlemail.com>
2015-01-22 12:53:31 +0000

    PEP8 formatting of variables, changed to sleep after successfully posting

--- __init__.py
+++ __init__.py
@@ -5,6 +5,12 @@ import time
 import pyteaser
 import traceback
 
+global posted_this_iteration
+
+# Configurables: ----------------
+sleep_time = 5*60
+#################################
+
 # Get the account password from the config stored with the bot
 config = ConfigParser.ConfigParser()
 config.read('account.cfg')
@@ -22,6 +28,8 @@ reddit.login(username, password)
 
 alreadyDone = set()
 
+print 'TLDRify online - Logged in and running'
+
 
 def tldr_already(text):
     if re.findall('((i)T[^\w]*L[^\w]*D[^\w]*R)', text).__len__() > 0:
@@ -31,29 +39,40 @@ def tldr_already(text):
 
 
 def create_summaries(title, text):
-    summaries = pyteaser.Summarize(title, text)
-    formatted_summary = title + u':\n'
+    try:
+        summaries = pyteaser.Summarize(title, text)
+    except Exception as e:
+        print e
+        print 'No Summary Could Be Generated'
+        return None
+    if not summaries:
+        print 'No Summary Could Be Generated'
+        return None
+    formatted_summary = u'##TLDR: \n\n' + title + u':\n\n'
     for summary in summaries:
-        formatted_summary += u'- ' + summary + u'\n'
+        formatted_summary += u'- ' + summary + u'\n\n'
     return formatted_summary
 
 
 def create_summaries_from_url(title, url):
     try:
         summaries = pyteaser.SummarizeUrl(url)
-    except:
-        return 'No Summary'
-    formatted_summary = title + u':\n'
+    except Exception as e:
+        print e
+        print 'No Summary Could Be Generated'
+        return None
+    if not summaries:
+        print 'No Summary Could Be Generated'
+        return None
+    formatted_summary = u'##TLDR: \n\n' + title + u':\n\n'
     for summary in summaries:
-        formatted_summary += u'- ' + summary + u'\n'
+        formatted_summary += u'- ' + summary + u'\n\n'
     return formatted_summary
-
-
-print 'TLDRify online - Logged in and running'
 
 
 def main():
     subreddit = reddit.get_subreddit('testingground4bots')
+    global posted_this_iteration
     for submission in subreddit.get_hot(limit=20):
         try:
             print 'Post Title', submission.title
@@ -70,9 +89,11 @@ def main():
                 if summary.__len__() > 1200:
                     print 'Summary Length:' , summary.__len__()
                     print 'Rejected for length exceeded'
-                msg = '##TLDR: \n' + summary
+                msg = summary
                 submission.add_comment(msg)
-                print 'Posted a TLDR successfully'
+                posted_this_iteration = True
+                print 'Posted a Link Post TLDR successfully:', submission.title,
+                return
             else:
                 op_text = submission.selftext
                 if not (tldr_already(op_text)) and op_text.__len__() > 1000:
@@ -84,26 +105,30 @@ def main():
                     if summary.__len__() > 750 and 'No Summary' not in summary:
                         print 'Summary Length:' , summary.__len__()
                         print 'Rejected for length exceeded'
-                    msg = '##TLDR: \n' + summary
+                    msg = summary
                     submission.add_comment(msg)
-                    print 'Posted a TLDR successfully'
+                    posted_this_iteration = True
+                    print 'Posted a TLDR successfully', submission.title
+                    return
         alreadyDone.add(submission.id)
 
-sleepytime = 5
-
 while True:
+    global posted_this_iteration
+    posted_this_iteration = False
     try:
         main()
-        if (sleepytime>(7*60)):
-            sleepytime = round(sleepytime/2)
+        if sleep_time > (7*60):
+            sleep_time = round(sleep_time/2)
             print 'Sleeping for %d seconds between runs'
     except Exception as e:
-        #if not successful, slow down.
-        if (str(e)=="HTTP Error 504: Gateway Time-out" or str(e)=="timed out"):
-            sleepytime = round(sleepytime*2)
-            print 'Sleeping for %d seconds between runs'
+        # if not successful, slow down.
+        if str(e) == "HTTP Error 504: Gateway Time-out" or str(e) == "timed out" or "RateLimitExceeded" in str(e):
+            sleep_time = round(sleep_time*2)
+            print 'Sleeping for %d seconds between runs' % sleep_time
         else:
             print 'Exception:', e
             traceback.print_exc()
             pass
-    time.sleep(sleepytime)
+    if posted_this_iteration:
+        posted_this_iteration = False
+        time.sleep(sleep_time)
